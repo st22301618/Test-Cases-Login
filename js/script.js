0e79e61be9977e132aad051ba3fd297a8bbe2c23
Nikolai <22301618@student.ciu.edu.tr>
2026-04-14 09:48:01 +0300

    Refactoring script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
+
+// Selected elements from DOM
 const loginForm = document.getElementById('loginForm');
 const errorMessage = document.getElementById('errorMessage');
 
-
+// Validation Logic
 loginForm.addEventListener('submit', (e) => {
+    // Prevent the page from refreshing automatically
     e.preventDefault();
     
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value;
 
-    // Clear previous errors
+    // Clear previous error message
     errorMessage.textContent = "";
 
-    // 1. Check Empty Fields
+    // Check Empty Fields
     if (!email || !password) {
         errorMessage.textContent = "All fields are required.";
         return;
